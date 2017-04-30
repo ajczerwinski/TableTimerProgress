8fadf85de097f2e049ea02eeb8abe07c1a0a0933
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-04-29 18:15:47 -0700

    faddd views for score display and jobs, create and connect new JobsVC

---
+++ JobsVC.swift
@@ -0,0 +1,24 @@
+//
+//  JobsVC.swift
+//  TableTimerProgress
+//
+//  Created by Allen Czerwinski on 4/29/17.
+//  Copyright © 2017 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+
+class JobsVC: UIViewController {
+    
+    @IBOutlet weak var scoreLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
