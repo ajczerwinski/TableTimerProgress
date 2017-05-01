6325ed1de3fa3037b573c9d18ce96f8deb80b31c
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-05-01 16:10:06 -0700

    add arrays for holding base and multiplier amounts for each role

--- JobsVC.swift
+++ JobsVC.swift
@@ -29,14 +29,15 @@ class JobsVC: UIViewController {
     // var timesTapped: Float = 0.0
     var timer = Timer()
     var tenths = 100
-    var score = 0.0
-    var multiplier: Float = 1.0
+    var score: Float = 0.0
     
     var roleImages = [#imageLiteral(resourceName: "studentDev"), #imageLiteral(resourceName: "internDev"), #imageLiteral(resourceName: "juniorDev"), #imageLiteral(resourceName: "dev"), #imageLiteral(resourceName: "seniorDev"), #imageLiteral(resourceName: "leadDev"), #imageLiteral(resourceName: "staffEng"), #imageLiteral(resourceName: "seniorStaffEng"), #imageLiteral(resourceName: "distinguishedEng"), #imageLiteral(resourceName: "superDistinguishedEng")]
     
     // store # of times tapped for each role
     
     var timesTapped: [Float] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var multiplier: [Float] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    var roleBase: [Float] = [1, 5, 10, 20, 50, 100, 500, 1000, 2000, 10000]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,6 +63,7 @@ class JobsVC: UIViewController {
             
         case 0:
             if progressBar[tag].progress == 1.0 {
+                score += multiplier[tag] * roleBase[tag]
                 scoreLbl.text = moneyFormatter(amount: Float(score))
                 progressBar[tag].progress = 0
                 
