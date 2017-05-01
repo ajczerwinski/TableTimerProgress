bda908d5fe5d2496e325b4e9a8a2b14d0d2440cb
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-04-30 17:58:42 -0700

    create and connect images to VC and format them as circles

--- JobsVC.swift
+++ JobsVC.swift
@@ -11,15 +11,64 @@ import UIKit
 
 class JobsVC: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var scoreLbl: UILabel!
+    
+    @IBOutlet weak var studentDevImg: UIImageView!
+    @IBOutlet weak var internDevImg: UIImageView!
+    @IBOutlet weak var juniorDevImg: UIImageView!
+    @IBOutlet weak var devImg: UIImageView!
+    @IBOutlet weak var seniorDevImg: UIImageView!
+    @IBOutlet weak var leadDevImg: UIImageView!
+    @IBOutlet weak var staffEngImg: UIImageView!
+    @IBOutlet weak var seniorStaffEngImg: UIImageView!
+    @IBOutlet weak var distinguishedEngImg: UIImageView!
+    @IBOutlet weak var superDistEngImg: UIImageView!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        formatImages()
         
     }
     
+    func formatImages() {
+        
+        studentDevImg.layer.cornerRadius = studentDevImg.frame.height / 2
+        studentDevImg.clipsToBounds = true
+        
+        internDevImg.layer.cornerRadius = internDevImg.frame.height / 2
+        internDevImg.clipsToBounds = true
+        
+        juniorDevImg.layer.cornerRadius = juniorDevImg.frame.height / 2
+        juniorDevImg.clipsToBounds = true
+        
+        devImg.layer.cornerRadius = devImg.frame.height / 2
+        devImg.clipsToBounds = true
+        
+        seniorDevImg.layer.cornerRadius = seniorDevImg.frame.height / 2
+        seniorDevImg.clipsToBounds = true
+        
+        leadDevImg.layer.cornerRadius = leadDevImg.frame.height / 2
+        leadDevImg.clipsToBounds = true
+        
+        staffEngImg.layer.cornerRadius = staffEngImg.frame.height / 2
+        staffEngImg.clipsToBounds = true
+        
+        seniorStaffEngImg.layer.cornerRadius = seniorStaffEngImg.frame.height / 2
+        seniorStaffEngImg.clipsToBounds = true
+        
+        distinguishedEngImg.layer.cornerRadius = distinguishedEngImg.frame.height / 2
+        distinguishedEngImg.clipsToBounds = true
+        
+        superDistEngImg.layer.cornerRadius = superDistEngImg.frame.height / 2
+        superDistEngImg.clipsToBounds = true
+        
+        //progressBar.progress = 0.0
+        
+    }
     
 }
