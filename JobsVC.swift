afeb307da515f57cfb1f5080841bdc83fa640a87
Allen Czerwinski <allen.czerwinski@gmail.com>
2017-04-30 22:52:24 -0700

    refactor image display, progressBars

--- JobsVC.swift
+++ JobsVC.swift
@@ -18,38 +18,25 @@ class JobsVC: UIViewController {
     
     // UI Images
     
-    @IBOutlet weak var studentDevImg: UIImageView!
-    @IBOutlet weak var internDevImg: UIImageView!
-    @IBOutlet weak var juniorDevImg: UIImageView!
-    @IBOutlet weak var devImg: UIImageView!
-    @IBOutlet weak var seniorDevImg: UIImageView!
-    @IBOutlet weak var leadDevImg: UIImageView!
-    @IBOutlet weak var staffEngImg: UIImageView!
-    @IBOutlet weak var seniorStaffEngImg: UIImageView!
-    @IBOutlet weak var distinguishedEngImg: UIImageView!
-    @IBOutlet weak var superDistEngImg: UIImageView!
+    @IBOutlet var roleImg: [UIImageView]!
     
     
     // Progress Bars
     
-    @IBOutlet weak var studentProgress: UIProgressView!
-    @IBOutlet weak var internProgress: UIProgressView!
-    @IBOutlet weak var juniorProgress: UIProgressView!
-    @IBOutlet weak var devProgress: UIProgressView!
-    @IBOutlet weak var seniorProgress: UIProgressView!
-    @IBOutlet weak var leadProgress: UIProgressView!
-    @IBOutlet weak var staffProgress: UIProgressView!
-    @IBOutlet weak var seniorStaffProgress: UIProgressView!
-    @IBOutlet weak var distinguishedProgress: UIProgressView!
-    @IBOutlet weak var superDistProgress: UIProgressView!
+    @IBOutlet var progressBar: [UIProgressView]!
     
     
-    var timesTapped: Float = 0.0
+    // var timesTapped: Float = 0.0
     var timer = Timer()
     var tenths = 100
     var score = 0.0
     var multiplier: Float = 1.0
     
+    var roleImages = [#imageLiteral(resourceName: "studentDev"), #imageLiteral(resourceName: "internDev"), #imageLiteral(resourceName: "juniorDev"), #imageLiteral(resourceName: "dev"), #imageLiteral(resourceName: "seniorDev"), #imageLiteral(resourceName: "leadDev"), #imageLiteral(resourceName: "staffEng"), #imageLiteral(resourceName: "seniorStaffEng"), #imageLiteral(resourceName: "distinguishedEng"), #imageLiteral(resourceName: "superDistinguishedEng")]
+    
+    // store # of times tapped for each role
+    
+    var timesTapped: [Float] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,121 +44,31 @@ class JobsVC: UIViewController {
         formatImages()
         
     }
+
     
-    
-    @IBAction func studentDevBtn(_ sender: UIButton) {
+    @IBAction func gameButton(_ sender: UIButton) {
         
-        timesTapped += 0.1
+        let buttonTag = sender.tag
         
+        timesTapped[buttonTag] += 0.1
         
-        if studentProgress.progress == 1.0 {
-            score += 1.0
-            scoreLbl.text = moneyFormatter(amount: Float(score))
-            studentProgress.progress = 0.0
-            timesTapped = 0.0
+        switch buttonTag {
+            
+        case 0:
+            if progressBar[buttonTag].progress == 1.0 {
+                scoreLbl.text = moneyFormatter(amount: Float(score))
+                progressBar[buttonTag].progress = 0
+                
+            }
+            progressBar[buttonTag].progress += 0.1
+            
+            break
+        case 1:
+            progressBar[buttonTag].progress += 0.1
+            
+        default:
+            return
         }
-        
-        studentProgress.progress = timesTapped
-        
-    }
-    
-    @IBAction func internDevBtn(_ sender: UIButton) {
-        
-        timesTapped += 0.1
-        
-        
-        if internProgress.progress == 1.0 {
-            score += 5.0
-            scoreLbl.text = moneyFormatter(amount: Float(score))
-            internProgress.progress = 0.0
-            timesTapped = 0.0
-        }
-        
-        internProgress.progress = timesTapped
-        
-    }
-    
-    @IBAction func juniorDevBtn(_ sender: UIButton) {
-    }
-    
-    @IBAction func devBtn(_ sender: UIButton) {
-    }
-    
-    @IBAction func seniorDevBtn(_ sender: UIButton) {
-    }
-    
-    @IBAction func leadDevBtn(_ sender: UIButton) {
-    }
-    
-    @IBAction func staffEngBtn(_ sender: UIButton) {
-    }
-    
-    @IBAction func seniorStaffEngBtn(_ sender: UIButton) {
-    }
-    
-    @IBAction func distinguishedEngBtn(_ sender: UIButton) {
-    }
-    
-    @IBAction func superDistinguishedEngBtn(_ sender: UIButton) {
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /* func gameButton() {
-        
-        timesTapped += 0.1
-        
-        
-        if progressBar.progress == 1.0 {
-            money += 1.0
-            moneyLbl.text = moneyFormatter(amount: Float(money))
-            progressBar.progress = 0.0
-            timesTapped = 0.0
-        }
-        
-        progressLbl.text = tapperProgressFormatter(text: timesTapped)
-        progressBar.progress = timesTapped
-        
-    } */
-    
-    
-    func formatImages() {
-        
-        studentDevImg.layer.cornerRadius = studentDevImg.frame.height / 2
-        studentDevImg.clipsToBounds = true
-        
-        internDevImg.layer.cornerRadius = internDevImg.frame.height / 2
-        internDevImg.clipsToBounds = true
-        
-        juniorDevImg.layer.cornerRadius = juniorDevImg.frame.height / 2
-        juniorDevImg.clipsToBounds = true
-        
-        devImg.layer.cornerRadius = devImg.frame.height / 2
-        devImg.clipsToBounds = true
-        
-        seniorDevImg.layer.cornerRadius = seniorDevImg.frame.height / 2
-        seniorDevImg.clipsToBounds = true
-        
-        leadDevImg.layer.cornerRadius = leadDevImg.frame.height / 2
-        leadDevImg.clipsToBounds = true
-        
-        staffEngImg.layer.cornerRadius = staffEngImg.frame.height / 2
-        staffEngImg.clipsToBounds = true
-        
-        seniorStaffEngImg.layer.cornerRadius = seniorStaffEngImg.frame.height / 2
-        seniorStaffEngImg.clipsToBounds = true
-        
-        distinguishedEngImg.layer.cornerRadius = distinguishedEngImg.frame.height / 2
-        distinguishedEngImg.clipsToBounds = true
-        
-        superDistEngImg.layer.cornerRadius = superDistEngImg.frame.height / 2
-        superDistEngImg.clipsToBounds = true
         
     }
     
@@ -183,5 +80,17 @@ class JobsVC: UIViewController {
         return formatter.string(from: NSNumber(value: amount))!
         
     }
+    
+    func formatImages() {
+        
+        for role in 0...9 {
+            roleImg[role].image = roleImages[role]
+            roleImg[role].layer.cornerRadius = roleImg[role].frame.height / 2
+            roleImg[role].clipsToBounds = true
+        }
+        
+    }
+    
+    
     
 }
